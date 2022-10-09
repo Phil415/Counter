e88eed098d0365ef2faff061089e4708fb8793c8
Phil415 <phillevy415@gmail.com>
2022-10-08 21:57:16 -0700

    Counter Commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, session, redirect, url_for
+app=Flask(__name__)
+app.secret_key = 'random1234'
+
+@app.route('/')
+def home ():
+    if 'count' in session:
+        session['count'] = session['count'] + 1
+    else:
+        session['count'] = 1
+    # return str(session['count'])
+    return render_template("home.html", count=session['count'])
+
+@app.route('/destroy_session')
+def destroy_session ():
+    session.pop("count")
+    return redirect(url_for('home'))
+
+app.run(debug=True)
